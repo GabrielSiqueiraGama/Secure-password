3f391d8464e99fc213a4b4bc7b40af1a965fd5f6
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-07-03 22:25:49 -0300

    feat: :seedling: adding NotNullCondition for secure password

--- src/main/java/zhant/securepassword/services/PasswordService.java
+++ src/main/java/zhant/securepassword/services/PasswordService.java
@@ -12,11 +12,16 @@ public class PasswordService {
 	public List<String> listOfFails(String password){
 		List<String> failures = new ArrayList<String>();
 		
+		validateIsNotNull(password,failures);
+		if (!failures.isEmpty()) {//Para não retornar exceção é preciso já retornar a badrequest, se não dar erro no servidor
+			return failures;
+		}
 		validateLength(password, failures);
 		validateUppercaseLetter(password, failures);
 		validateLowercaseLetter(password, failures);
 		validateNumber(password, failures);
 		validateEspecialCaracter(password, failures);
+		
 		
 		return failures;
 	}
@@ -44,11 +49,14 @@ public class PasswordService {
 			failures.add("A senha deve conter pelo menos um caractere especial.");
 		}
 	}
-
-	private void validateLength(String password, List<String> failures) {
-		if(password == null) {
+	
+	private void validateIsNotNull(String password, List<String> failures) {
+		if(password == null || password == "" || password.isEmpty()) {
 			failures.add("A senha não pode ser nula.");
 		}
+	}
+
+	private void validateLength(String password, List<String> failures) {
 		if(password.length() < 8) {
 			failures.add("Senha possui que menos 8 digitos.");
 		}
